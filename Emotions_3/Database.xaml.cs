9c155a235374191f09b07428534530f30ec64c31
Well 1306 <well1306@list.ru>
2022-11-17 21:06:08 +0300

    Lab3

---
+++ Emotions_3/Database.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Emotions_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Database.xaml
+    /// </summary>
+    public partial class Database : Window
+    {
+        private ICommand _deleteCommand;
+        public ICommand DeleteCommand => _deleteCommand ?? (_deleteCommand = new RelayCommand(parameter =>
+        {
+            var image = Collection[ImagesCollectionListBox.SelectedIndex];
+            using (var db = new DB())
+            {
+                var deletedImage = db.Pic.Where(x => x.Id == image.Id).First();
+                if (deletedImage == null)
+                {
+                    return;
+                }
+                db.Pic.Remove(deletedImage);
+                db.SaveChanges();
+                Collection.Remove(image);
+            }
+        }));
+        public ObservableCollection<DBpic> Collection { get; private set; }
+        public Database()
+        {
+            Collection = new ObservableCollection<DBpic>();
+
+            using (var db = new DB())
+            {
+                foreach (var v in db.Pic)
+                {
+                    Collection.Add(v);
+                }
+            }
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Drop(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var image = Collection[ImagesCollectionListBox.SelectedIndex];
+                using (var db = new DB())
+                {
+                    var deletedImage = db.Pic.Where(x => x.Id == image.Id).First();
+                    if (deletedImage == null)
+                    {
+                        return;
+                    }
+                    db.Pic.Remove(deletedImage);
+                    db.SaveChanges();
+                    Collection.Remove(image);
+                }
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+        }
+    }
+}
---
+++ Emotions_3/Picture.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotions_2
+{
+    public class Picture : INotifyPropertyChanged
+    {
+        private int number;
+        public int Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public List<Tuple<string, float>> Emotions { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DBpic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public byte[] Img { get; set; }
+        public string Emotions { get; set; }
+
+        public static string GetHash(byte[] data)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return string.Concat(sha256.ComputeHash(data).Select(x => x.ToString("X2")));
+            }
+        }
+    }
+
+    public class DB : DbContext
+    {
+        public DbSet<DBpic> Pic { get; set; }
+
+        public DB()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+        {
+            o.UseSqlite("Data Source=emotions.db");
+        }
+    }
+}
