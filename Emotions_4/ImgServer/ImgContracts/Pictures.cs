1fd5ee88864cd3960a2f5814ee7682395ad994ee
Well 1306 <well1306@list.ru>
2022-12-09 00:07:52 +0300

    lab4

---
+++ Emotions_4/ImgServer/ImgContracts/Pictures.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class Picture : INotifyPropertyChanged
+    {
+        private int number;
+        public int Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public List<Tuple<string, float>> Emotions { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DBpic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public byte[] Img { get; set; }
+        public string Emotions { get; set; }
+
+        public static string GetHash(byte[] data)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return string.Concat(sha256.ComputeHash(data).Select(x => x.ToString("X2")));
+            }
+        }
+    }
+
+    public class DB : DbContext
+    {
+        public DbSet<DBpic> Pic { get; set; }
+        private string path = "..//..//..//..//emotions.db";
+
+        public DB(string p = "..//..//..//..//emotions.db")
+        {
+            path = p;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+        {
+            o.UseSqlite($"Data Source={path}");
+        }
+    }
+
+
+    public class GITT
+    {
+        public static DenseTensor<float> GrayscaleImageToTensor(Image<Rgb24> img)
+        {
+            var w = img.Width;
+            var h = img.Height;
+            var t = new DenseTensor<float>(new[] { 1, 1, h, w });
+
+            img.ProcessPixelRows(pa =>
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+                    for (int x = 0; x < w; x++)
+                    {
+                        t[0, 0, y, x] = pixelSpan[x].R; // B and G are the same
+                    }
+                }
+            });
+
+            return t;
+        }
+
+        public static List<Tuple<string, float>> StringToTuple(string s)
+        {
+            List<Tuple<string, float>> r = new();
+            List<string> str = s.Split('\n').ToList();
+            str.RemoveAt(str.Count - 1);
+            foreach (var v in str)
+            {
+                r.Add(Tuple.Create(v.Split(":")[0], float.Parse(v.Split(" ")[1])));
+            }
+            return r;
+        }
+        public static bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+
+            if (file1 == file2)
+            {
+                return true;
+            }
+
+            fs1 = new FileStream(file1, FileMode.Open);
+            fs2 = new FileStream(file2, FileMode.Open);
+
+            if (fs1.Length != fs2.Length)
+            {
+                fs1.Close();
+                fs2.Close();
+
+                return false;
+            }
+
+            do
+            {
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            fs1.Close();
+            fs2.Close();
+
+            return ((file1byte - file2byte) == 0);
+        }
+    }
+}
---
+++ Emotions_4/ImgServer/ImgServer/Database.cs
@@ -0,0 +1,130 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.Text.Json;
+
+namespace ImgServer
+{
+    public class DB : DbContext
+    {
+        public DbSet<DBpic> Pic { get; set; }
+
+        public DB()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+        {
+            o.UseSqlite($"Data Source=emotions.db");
+        }
+    }
+
+    public interface I_Db
+    {
+        Task<List<int>> GetAllImages();
+        Task<string> PostImage(string path, CancellationToken token);
+        Task<DBpic> TryGetImageById(int id);
+        Task<int> TryDeleteImage(int id);
+    }
+
+    public class ImagesDatabase : I_Db
+    {
+        public async Task<List<int>> GetAllImages()
+        {
+            var result = new List<int>();
+            try
+            {
+                using (var db = new DB())
+                {
+                    var query = db.Pic;
+                    foreach (var image in query)
+                    {
+                        result.Add(image.Id);
+                    }
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return result;
+            }
+        }
+
+        public async Task<DBpic> TryGetImageById(int id)
+        {
+            using (var db = new DB())
+            {
+                var q = db.Pic.Where(x => x.Id == id).FirstOrDefault();
+                if(q is not null)
+                    return q;
+                else 
+                    return null;
+            }
+        }
+
+        public async Task<int> TryDeleteImage(int id)
+        {
+            using (var db = new DB())
+            {
+                var q = db.Pic.Where(x => x.Id == id).FirstOrDefault();
+                if (q is not null)
+                {
+                    db.Pic.Remove(q);
+                    db.SaveChanges();
+                    return 0;
+                }    
+                else
+                    return -1; 
+            }
+        }
+
+        public async Task<string> PostImage(string f, CancellationToken token)
+        {
+            using (var db = new DB())
+            {
+                string hash = DBpic.GetHash(System.IO.File.ReadAllBytes(f));
+                var q = db.Pic.Where(x => x.Hash == hash).FirstOrDefault();
+                if ((q is not null) && GITT.FileCompare(f, q.Name))
+                {
+                    string json = JsonSerializer.Serialize(q);
+                    return json; //если изображение есть в базе
+                }
+                else
+                {
+                    using Image<Rgb24> img = SixLabors.ImageSharp.Image.Load<Rgb24>(f);
+                    img.Mutate(ctx =>
+                    {
+                        ctx.Resize(new SixLabors.ImageSharp.Size(64, 64));
+                    });
+                    var inputs = GITT.GrayscaleImageToTensor(img);
+                    var a = new Emotions.EmotionsAsync();
+                    List<Tuple<string, float>> r = new();
+                    try
+                    {
+                        //count++;
+                        var res = await a.Emotions(inputs, token);
+                        r = res.OrderByDescending(t => t.Item2).ToList();
+                    }
+                    catch (OperationCanceledException ex) { return null; }
+                    string _e = "";
+                    foreach (var _ in r)
+                        _e += _.Item1 + ": " + _.Item2.ToString() + "\n";
+                    DBpic p = new DBpic
+                    {
+                        Name = f,
+                        Emotions = _e,
+                        Img = System.IO.File.ReadAllBytes(f),
+                        Hash = hash
+                    };
+                    db.Add(p);
+                    db.SaveChanges();
+                    string json = JsonSerializer.Serialize(p);
+                    return json;
+                }
+            }
+        }
+    }
+}
